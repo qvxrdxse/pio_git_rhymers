481b3c61942e7aeabdafe2e932a7802ffdf128a0
Kacper Kazimierczak <255622@edu.p.lodz.pl>
2026-04-13 14:39:37 +0200

    17. Add documentation to FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,16 +1,38 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa FIFORhymer implementuje mechanizm wyliczanki w kolejności FIFO
+ * (First-In-First-Out – pierwszy wchodzi, pierwszy wychodzi).
+ *
+ * Rozszerza klasę DefaultCountingOutRhymer i modyfikuje sposób zwracania elementów,
+ * tak aby zachowywały się jak kolejka, a nie stos.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer(); 
+    /**
+     * Tymczasowy obiekt pomocniczy używany do odwrócenia kolejności elementów.
+     */
+    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Zwraca element zgodnie z zasadą FIFO.
+     *
+     * Metoda przenosi wszystkie elementy do struktury tymczasowej,
+     * aby odwrócić ich kolejność, następnie pobiera pierwszy dodany element,
+     * a pozostałe elementy przywraca do oryginalnej struktury.
+     *
+     * @return element, który został dodany jako pierwszy
+     */
     @Override
     public int countOut() {
+        // Przeniesienie wszystkich elementów do struktury tymczasowej
         while (!callCheck())
             temp.countIn(super.countOut());
 
+        // Pobranie najstarszego elementu (FIFO)
         int ret = temp.countOut();
 
+        // Przywrócenie pozostałych elementów
         while (!temp.callCheck())
             countIn(temp.countOut());
 
